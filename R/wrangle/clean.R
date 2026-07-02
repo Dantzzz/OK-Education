1d9ca4ec6f6ca97cdc262f04ad465d79791bd04e
Dantz Farrow <dantzfarrow@gmail.com>
2026-07-01 22:32:32 -0500

    Add script subdirectories under R/ with .gitkeep temporary placeholders.

---
+++ R/wrangle/clean.R
@@ -0,0 +1,19 @@
+####################################
+# Title: Clean NCES Data
+# Author: Dantz Farrow
+# Last Modified: 07/01/2026
+####################################
+
+clean_nces <- function(file) {
+  # replace en-dash (unicode) w/ NAs on load
+  read_csv(file, na = c("", "NA", "\u2013")) %>%
+    # trim whitespace
+    mutate(operational_status = str_trim(operational_status)) %>%
+    # filter out closed/annexed districts
+    filter(operational_status == "Open") %>%
+    # enforce dtypes
+    mutate(across(fips:district, as.character)) %>%
+    mutate(across(enroll_0108:title_i, as.numeric)) %>%
+    # drop unnecessary columns
+    select(-state, -lea_charter)
+}
